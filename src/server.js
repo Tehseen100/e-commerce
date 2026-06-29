a1f0b330c1177fde5228e086dfda3351cb644a9c
Tehseen Javed <tehseenjaved100@gmail.com>
2026-06-29 19:59:48 +0500

    initial setup and db connection

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'; // Load environment variables from .env file
+import { app } from './app.js';
+import { connectDB } from './db/index.js';
+
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB   
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
